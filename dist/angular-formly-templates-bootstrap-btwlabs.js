9cbf3fb801569e32374b75003474b7b36f2da3d2
Ted Benice <tbenice@admins-mbp.pk5001z>
2016-08-30 08:34:47 -0700

    putting description before the inputs

--- dist/angular-formly-templates-bootstrap-btwlabs.js
+++ dist/angular-formly-templates-bootstrap-btwlabs.js
@@ -588,8 +588,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    formlyConfig.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
 	      if (angular.isDefined(options.templateOptions.description)) {
 	        var el = document.createElement('div');
-	        el.appendChild(angular.element(template)[0]);
 	        el.appendChild(angular.element('<p id="' + scope.id + '_description"' + 'class="help-block"' + 'ng-if="to.description">' + '{{to.description}}' + '</p>')[0]);
+	        el.appendChild(angular.element(template)[0]);
 	        var modelEls = angular.element(el.querySelectorAll('[ng-model]'));
 	        if (modelEls) {
 	          modelEls.attr('aria-describedby', scope.id + '_description');
--- src/run/description.js
+++ src/run/description.js
@@ -5,14 +5,14 @@ export default ngModule => {
     formlyConfig.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
       if (angular.isDefined(options.templateOptions.description)) {
         var el = document.createElement('div');
-        el.appendChild(angular.element(template)[0]);
         el.appendChild(angular.element(
-          '<p id="' + scope.id + '_description"' +
-          'class="help-block"' +
-          'ng-if="to.description">' +
-          '{{to.description}}' +
-          '</p>'
+            '<p id="' + scope.id + '_description"' +
+            'class="help-block"' +
+            'ng-if="to.description">' +
+            '{{to.description}}' +
+            '</p>'
         )[0]);
+        el.appendChild(angular.element(template)[0]);
         var modelEls = angular.element(el.querySelectorAll('[ng-model]'));
         if (modelEls) {
           modelEls.attr('aria-describedby', scope.id + '_description');
